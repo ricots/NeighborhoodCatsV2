11a6f06107a8f2f3ca4259c16d2f6d04ec47c816
roberrera <roberterrera@gmail.com>
2016-04-07 21:45:57 -0400

    Update MapsActivity so location does not constantly update

--- NeighborhoodCats/app/src/main/java/com/roberterrera/neighborhoodcats/activities/MapsActivity.java
+++ NeighborhoodCats/app/src/main/java/com/roberterrera/neighborhoodcats/activities/MapsActivity.java
@@ -13,8 +13,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.analytics.HitBuilders;
@@ -60,8 +58,6 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
         setContentView(R.layout.activity_maps);
 
         setTitle("Map Your Cats!");
-
-        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -225,27 +221,7 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.map, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onLocationChanged(Location location) {
-
-        double lat = location.getLatitude();
-        double lng = location.getLongitude();
-
-        Log.i("Location info: Lat", String.valueOf(lat));
-        Log.i("Location info: Lng", String.valueOf(lng));
-
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 18));
     }
 
 
